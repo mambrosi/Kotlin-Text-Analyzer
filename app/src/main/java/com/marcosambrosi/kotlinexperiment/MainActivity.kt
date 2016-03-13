9cdcbc89c8ed8effacc2d634179013221663ecee
Marcos Ambrosi <marcosambrosi@gmail.com>
2016-03-13 18:47:58 -0300

    Added Quoute model

--- app/src/main/java/com/marcosambrosi/kotlinexperiment/MainActivity.kt
+++ app/src/main/java/com/marcosambrosi/kotlinexperiment/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.marcosambrosi.kotlinexperiment
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val appName: String = "Test"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/marcosambrosi/kotlinexperiment/model/Model.kt
@@ -0,0 +1,9 @@
+package com.marcosambrosi.kotlinexperiment.model
+
+/**
+ * Created by marcosambrosi on 3/13/16.
+ */
+class Model {
+
+    data class Quote(val author: String, val quote: String)
+}
