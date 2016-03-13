80a1c71dead1e64d6d0a43ea6d1ce1641fbe8bc2
Marcos Ambrosi <marcosambrosi@gmail.com>
2016-03-13 19:07:12 -0300

    Changed comments to more generic ones

---
+++ app/src/main/java/com/marcosambrosi/kotlinexperiment/network/Api.kt
@@ -0,0 +1,9 @@
+package com.marcosambrosi.kotlinexperiment.network
+
+/**
+ * Created by marcosambrosi on 3/13/16.
+ */
+interface Api {
+
+    fun getQuote(key:Integer)
+}
---
+++ app/src/main/java/com/marcosambrosi/kotlinexperiment/network/ApiManager.kt
@@ -0,0 +1,10 @@
+package com.marcosambrosi.kotlinexperiment.network
+
+/**
+ * Created by marcosambrosi on 3/13/16.
+ */
+class ApiManager {
+
+    val baseUrl = "https://sentity-v1.p.mashape.com/v1/"
+
+}
